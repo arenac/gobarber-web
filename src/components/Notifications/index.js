f4a1a008cef84323a950831269ed02d65e45dc6f
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-20 23:45:28 +0100

    NotificationList, Notification components

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { MdNotifications } from 'react-icons/md';
 
-import { Container, Badge } from './styles';
+import { Container, Badge, NotificationList, Notification } from './styles';
 
 export default function Notifications() {
   return (
@@ -10,6 +10,39 @@ export default function Notifications() {
       <Badge hasUnread>
         <MdNotifications color="#7519c1" size={20} />
       </Badge>
+
+      <NotificationList>
+        <Notification unread>
+          <p>You have a new appoitment</p>
+          <time>2 days ago</time>
+          <button type="button">Set as read</button>
+        </Notification>
+        <Notification>
+          <p>You have a new appoitment</p>
+          <time>2 days ago</time>
+          <button type="button">Set as read</button>
+        </Notification>
+        <Notification>
+          <p>You have a new appoitment</p>
+          <time>2 days ago</time>
+          <button type="button">Set as read</button>
+        </Notification>
+        <Notification>
+          <p>You have a new appoitment</p>
+          <time>2 days ago</time>
+          <button type="button">Set as read</button>
+        </Notification>
+        <Notification>
+          <p>You have a new appoitment</p>
+          <time>2 days ago</time>
+          <button type="button">Set as read</button>
+        </Notification>
+        <Notification>
+          <p>You have a new appoitment</p>
+          <time>2 days ago</time>
+          <button type="button">Set as read</button>
+        </Notification>
+      </NotificationList>
     </Container>
   );
 }
