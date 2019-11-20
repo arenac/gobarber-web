b037b5988b12ce6a7afbb189dc8368e7c0bd092a
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-20 16:04:11 +0100

    listening signup request and dispatch success

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSuccess, signFailure } from './actions';
+import { signInSuccess, signUpSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
   try {
@@ -32,4 +32,28 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    yield put(signUpSuccess());
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Subscription failure');
+    console.tron.error(err);
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
