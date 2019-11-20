8f4598d14e54038ba76ddd4d9389292be5df3e03
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-21 00:57:47 +0100

    fetch notifications and handle visibility

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { parseISO, formatDistance } from 'date-fns';
+import en from 'date-fns/locale/en-US';
 
 import { MdNotifications } from 'react-icons/md';
+import api from '~/services/api';
 
 import {
   Container,
@@ -11,54 +14,49 @@ import {
 } from './styles';
 
 export default function Notifications() {
+  const [visible, setVisible] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  function handleToggleVisible() {
+    setVisible(!visible);
+  }
+
+  useEffect(() => {
+    async function loadNotifications() {
+      const response = await api.get('notifications');
+
+      const data = response.data.map(notification => ({
+        ...notification,
+        timeDistance: formatDistance(
+          parseISO(notification.createdAt),
+          new Date(),
+          {
+            addSuffix: true,
+            locale: en,
+          }
+        ),
+      }));
+
+      setNotifications(data);
+    }
+    loadNotifications();
+  }, []);
+
   return (
     <Container>
-      <Badge hasUnread>
+      <Badge hasUnread onClick={handleToggleVisible}>
         <MdNotifications color="#7519c1" size={20} />
       </Badge>
 
-      <NotificationList>
+      <NotificationList visible={visible}>
         <Scroll>
-          <Notification unread>
-            <p>You have a new appoitment</p>
-            <time>2 days ago</time>
-            <button type="button">Set as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appoitment</p>
-            <time>2 days ago</time>
-            <button type="button">Set as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appoitment</p>
-            <time>2 days ago</time>
-            <button type="button">Set as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appoitment</p>
-            <time>2 days ago</time>
-            <button type="button">Set as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appoitment</p>
-            <time>2 days ago</time>
-            <button type="button">Set as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appoitment</p>
-            <time>2 days ago</time>
-            <button type="button">Set as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appoitment</p>
-            <time>2 days ago</time>
-            <button type="button">Set as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appoitment</p>
-            <time>2 days ago</time>
-            <button type="button">Set as read</button>
-          </Notification>
+          {notifications.map(notification => (
+            <Notification key={notification._id} unread={!notification.read}>
+              <p>{notification.content}</p>
+              <time>{notification.timeDistance}</time>
+              <button type="button">Set as read</button>
+            </Notification>
+          ))}
         </Scroll>
       </NotificationList>
     </Container>
