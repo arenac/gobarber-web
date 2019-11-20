168f8fc6d1cbedb09b7bfb3bc22f45cc51ed34ab
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-20 01:24:04 +0100

    retrieve loading from state

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
@@ -19,6 +19,7 @@ const schema = Yup.object().shape({
 
 export default function SignIn() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -32,7 +33,7 @@ export default function SignIn() {
         <Input name="email" type="email" placeholder="Your e-mail" />
         <Input name="password" type="password" placeholder="Your password" />
 
-        <button type="submit">Log In</button>
+        <button type="submit">{loading ? 'Loading...' : 'Log In'}</button>
         <Link to="/register">Do you need an account?</Link>
       </Form>
     </>
