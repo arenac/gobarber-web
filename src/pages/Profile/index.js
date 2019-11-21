f25caa8d24e0c9d03173055393ed41e2f6debaff
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-21 23:53:16 +0100

    AvatartInput into profile form

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,6 +5,8 @@ import { Form, Input } from '@rocketseat/unform';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 import { Container } from './styles';
 
+import AvatarInput from './AvatarInput';
+
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
   const dispatch = useDispatch();
@@ -16,6 +18,7 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Full name" />
         <Input name="email" type="email" placeholder="Your e-mail" />
         <hr />
