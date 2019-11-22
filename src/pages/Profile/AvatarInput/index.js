d6510b478912034fe664c73942b886adcb1a5189
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-22 01:14:48 +0100

    show the avatar and update through the api when change img

--- src/pages/Profile/AvatarInput/index.js
+++ src/pages/Profile/AvatarInput/index.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useField } from '@rocketseat/unform';
+
+import api from '~/services/api';
 
 import { Container } from './styles';
 
 export default function AvatarInput() {
-  function handleChange(e) {}
+  /**
+   * useField will get the avatar property from (the user state) profile,
+   * initialData of Form component where the AvatarInput is in.
+   */
+  const { defaultValue, registerField } = useField('avatar');
+
+  const [file, setFile] = useState(defaultValue && defaultValue.id);
+  const [preview, setPreview] = useState(defaultValue && defaultValue.url);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    if (ref.current) {
+      registerField({
+        name: 'avatar_id',
+        ref: ref.current,
+        path: 'dataset.file',
+      });
+    }
+  }, [ref, registerField]);
+
+  async function handleChange(e) {
+    const data = new FormData();
+
+    data.append('file', e.target.files[0]); // e.target.files[0] will restrict to send a single file
+
+    const response = await api.post('files', data);
+
+    const { id, url } = response.data;
+
+    setFile(id);
+    setPreview(url);
+  }
+
   return (
     <Container>
       <label htmlFor="avatar">
         <img
-          src="https://api.adorable.io/avatars/50/abott@adorable.png"
+          src={
+            preview || 'https://api.adorable.io/avatars/50/abott@adorable.png'
+          }
           alt="Add your avatar"
         />
         <input
           type="file"
           id="avatar"
           accept="image/*"
+          data-file={file} // the unform will recover this info and use as reference
           onChange={handleChange}
+          ref={ref} // the unform will recover this info and use as reference
         />
       </label>
     </Container>
