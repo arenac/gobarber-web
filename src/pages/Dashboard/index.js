403f0a0bf304232bbc79a74cd22efdd75e89baac
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-22 14:06:18 +0100

    heaser layout

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight} from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+
+import { Container } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>May 31th</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+    </Container>
+  );
 }
